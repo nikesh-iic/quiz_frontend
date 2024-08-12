a0620fdbe3f2ca32db9f68096bc2b199843dd562
regmi07 <regmi.nikes1@gmail.com>
2024-08-12 09:11:24 +0545

    styled register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,63 @@
 const Register = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <div>
-        <h1>Register</h1>
-        <div>
+      <div className="p-4 space-y-6 bg-white w-96">
+        <h1 className="text-2xl text-center font-bold p-2 text-gray-700">
+          Register
+        </h1>
+        <div className="space-y-7 mt-8">
           <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" />
+            <label
+              htmlFor="username"
+              className="block font-medium text-gray-700"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              className="mt-3 p-2 w-full border"
+            />
           </div>
           <div>
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" />
+            <label htmlFor="email" className="block font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              className="mt-3 p-2 w-full border"
+            />
           </div>
           <div>
-            <label htmlFor="password1">Password</label>
-            <input type="password" name="password1" />
+            <label
+              htmlFor="password1"
+              className="block font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password1"
+              className="mt-3 p-2 w-full border"
+            />
           </div>
           <div>
-            <label htmlFor="password2">Confirm Password</label>
-            <input type="password" name="password2" />
+            <label
+              htmlFor="password2"
+              className="block font-medium text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="password2"
+              className="mt-3 p-2 w-full border"
+            />
           </div>
-          <button>Register</button>
+          <button className="p-2 w-full border bg-indigo-500 text-white">
+            Register
+          </button>
         </div>
       </div>
     </div>
